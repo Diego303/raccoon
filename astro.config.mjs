849c3f85c36e1fcde8cbe7b217bacd03d91f7be0
Diego Alba Ruiz <diego303.d@gmail.com>
2025-12-24 01:24:42 +0100

    Actualizada ruta de imagenes por problema

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Diego303.github.io",
-  base: "raccoon", 
+  base: "/raccoon/", 
 });
